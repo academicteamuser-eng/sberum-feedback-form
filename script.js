e335a4c68b7a988ad431dd305475244516e45293
academicteamuser-eng <academic.team.user@gmail.com>
2025-12-19 12:36:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -243,7 +243,7 @@ function createInfoIcons() {
         // Создаем иконку
         const icon = document.createElement('span');
         icon.className = 'info-icon';
-       // icon.textContent = 'ℹ️';
+       icon.textContent = 'i';
         icon.title = 'Показать подсказку';
         icon.setAttribute('aria-label', 'Информация');
         icon.setAttribute('role', 'button');
